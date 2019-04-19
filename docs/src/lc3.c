7f36a2a7bf54b38cef642fe57b6ed775206cd380
mhashim6 <m6hashim@gmail.com>
2019-04-19 09:24:59 +0200

    simplified TRAP_IN with putc.

--- docs/src/lc3.c
+++ docs/src/lc3.c
@@ -69,10 +69,10 @@ enum
 /* TRAP Codes */
 enum
 {
-    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal. */
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
     TRAP_OUT = 0x21,   /* output a character */
     TRAP_PUTS = 0x22,  /* output a word string */
-    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal. */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
     TRAP_PUTSP = 0x24, /* output a byte string */
     TRAP_HALT = 0x25   /* halt the program */
 };
@@ -454,9 +454,9 @@ int main(int argc, const char* argv[])
                     case TRAP_IN:
                         /* TRAP IN */
                         printf("Enter a character: ");
-                        restore_input_buffering();
-                        reg[R_R0] = (uint16_t)getchar();
-                        disable_input_buffering();
+                        char c = getchar();
+                        putc(c, stdout);
+                        reg[R_R0] = (uint16_t)c;
                         break;
                     case TRAP_PUTSP:
                         /* TRAP PUTSP */
