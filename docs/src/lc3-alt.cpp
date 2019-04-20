d026696d42d6ea87d8439d4f837375366891be2e
Ryan Pendleton <me@ryanp.me>
2019-04-20 13:46:18 -0600

    update generated sources to match .lit document

--- docs/src/lc3-alt.cpp
+++ docs/src/lc3-alt.cpp
@@ -70,10 +70,10 @@ enum
 /* TRAP Codes */
 enum
 {
-    TRAP_GETC = 0x20,  /* get character from keyboard */
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
     TRAP_OUT = 0x21,   /* output a character */
     TRAP_PUTS = 0x22,  /* output a word string */
-    TRAP_IN = 0x23,    /* input a string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
     TRAP_PUTSP = 0x24, /* output a byte string */
     TRAP_HALT = 0x25   /* halt the program */
 };
@@ -325,7 +325,9 @@ void ins(uint16_t instr)
              case TRAP_IN:
                  /* TRAP IN */
                  printf("Enter a character: ");
-                 reg[R_R0] = (uint16_t)getchar();
+                 char c = getchar();
+                 putc(c, stdout);
+                 reg[R_R0] = (uint16_t)c;
 
                  break;
              case TRAP_PUTSP:
