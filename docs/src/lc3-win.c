a66f9e4d6ec960b534a636002e8dd72f64667be2
woclass <inkydragon@users.noreply.github.com>
2020-02-10 16:09:00 +0800

    group includes; re-gen c/cpp & html

--- docs/src/lc3-win.c
+++ docs/src/lc3-win.c
@@ -1,9 +1,10 @@
 /* lc3-win.c */
 /* Windows Includes */
-#include <Windows.h>
 #include <stdint.h> // uint16_t
 #include <stdio.h>  // FILE
 #include <signal.h> // SIGINT
+/* windows only */
+#include <Windows.h>
 #include <conio.h>  // _kbhit
 
 HANDLE hStdin = INVALID_HANDLE_VALUE;
